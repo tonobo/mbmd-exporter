dfb1a1f76e7231c33a811b0b392958cf065bfc55
Tim Foerster <tim.foerster@hetzner.com>
2024-01-19 22:40:05 +0100

    Add hass mqtt discovery for mbmd values learned from mqtt and add victron grid reporter

--- Gemfile
+++ Gemfile
@@ -5,5 +5,6 @@ git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 gem 'sinatra'
 gem 'typhoeus'
 gem 'oj'
+gem 'mqtt'
 gem 'webrick'
 
--- app.rb
+++ app.rb
@@ -2,8 +2,123 @@ require 'sinatra'
 require 'typhoeus'
 require 'oj'
 require 'time'
+require 'mqtt'
+require 'logger'
+require 'time'
+
+$LOGGER = Logger.new(STDERR)
 
 URL = ENV.fetch('URL')
+
+if ENV["ENABLE_VICTRON_GRID_MQTT"]
+  SLEEP_TIME = 5 
+
+  Thread.new do
+    client = MQTT::Client.connect(ENV.fetch("MQTT_HOST"))
+
+    loop do
+      start = Time.now
+      response = Typhoeus::Request.new("#{URL}/api/last", timeout: 3).run
+      unless response.success?
+        raise "failed to contact mbmd: #{response}" 
+      end
+      json = Oj.safe_load(response.body)
+      json.each do |device_id, values|
+        topic = "mbmd-exporter/#{device_id}/metrics".downcase
+        data = {
+          grid: {
+            voltage: values.fetch("VoltageL1", 0),
+            power: values.fetch("PowerL1", 0) +
+            values.fetch("PowerL2", 0) + 
+            values.fetch("PowerL3", 0),
+            current: values.fetch("CurrentL1", 0) +
+            values.fetch("CurrentL2", 0) + 
+            values.fetch("CurrentL3", 0),
+            energy_forward: values.fetch("Export", 0),
+            energy_reverse: values.fetch("Import", 0),
+            L1: {
+              power: values.fetch("PowerL1", 0),
+              voltage: values.fetch("VoltageL1", 0),
+              current: values.fetch("CurrentL1", 0),
+              energy_forward: values.fetch("ExportL1", 0),
+              energy_reverse: values.fetch("ImportL1", 0),
+            },
+            L2: {
+              power: values.fetch("PowerL2", 0),
+              voltage: values.fetch("VoltageL2", 0),
+              current: values.fetch("CurrentL2", 0),
+              energy_forward: values.fetch("ExportL2", 0),
+              energy_reverse: values.fetch("ImportL2", 0),
+            },
+            L3: {
+              power: values.fetch("PowerL3", 0),
+              voltage: values.fetch("VoltageL3", 0),
+              current: values.fetch("CurrentL3", 0),
+              energy_forward: values.fetch("ExportL3", 0),
+              energy_reverse: values.fetch("ImportL3", 0),
+            }
+          }
+        }
+        client.publish(topic, Oj.dump(data, mode: :compat))
+      end
+      duration = Time.now - start
+      $LOGGER.info "Data collection took #{duration.to_f.round(3)} seconds"
+      sleep(SLEEP_TIME - duration)
+    end
+  rescue StandardError => error
+    $LOGGER.error error
+  ensure
+    # exit programm in case of failures
+    exit(1)
+  end
+end
+
+if ENV["ENABLE_HASS_DISCOVERY"]
+  # ENV MQTT_HOST required
+  Thread.new do
+    cache = {}
+    lookup_table = {
+      Export:         { unit: :Wh,  t: :total_increasing, c: :energy },
+      Import:         { unit: :Wh,  t: :total_increasing, c: :energy },
+      Sum:            { unit: :kWh, t: :total_increasing, c: :energy },
+      Cosphi:         {             t: :measurement,      c: :power_factor },
+      Current:        { unit: :A,   t: :measurement,      c: :current },
+      Voltage:        { unit: :V,   t: :measurement,      c: :voltage },
+      Power:          { unit: :W,   t: :measurement,      c: :power },
+      ReactivePower:  { unit: :var, t: :measurement,      c: :reactive_power },
+      Frequency:      { unit: :Hz,  t: :measurement,      c: :frequency },
+    }
+
+    client = MQTT::Client.connect(ENV.fetch("MQTT_HOST"))
+    client.subscribe(ENV.fetch("MQTT_TOPIC", "mbmd/#"))
+    client.get do |topic, message|
+      _prefix, device, kind, phase, _other = *topic.split("/")
+      kind = kind.to_s.to_sym
+      raise "_other filled, unsupported message -> topic: #{topic}, message: #{message}" if _other
+      next if device == "status" # skip status message
+      next if phase.to_s.match?(/^T\d+$/) # skip tarif counter
+      next unless lookup_table[kind]
+      
+      data = {
+        name: [kind, (phase || "Total")].join(" "),
+        stat_t: topic,
+        uniq_id: "mbmd-exporter-bridge_#{device.gsub(".","-")}_#{[kind, phase].compact.join("-").downcase}",
+        dev_cla: lookup_table.dig(kind, :c), 
+        stat_cla: lookup_table.dig(kind, :t),
+        exp_aft: 10,
+        dev: { name: device, ids: device.gsub(".","-"), cu: URL, mf: "mbmd", mdl: device },
+      }
+
+      next if cache[data[:uniq_id]] # already published, skipping
+      cache[data[:uniq_id]] = true
+
+      data[:unit_of_meas] = lookup_table.dig(kind, :unit) if lookup_table.dig(kind, :unit) 
+      key = "homeassistant/sensor/mbmd-exporter-bridge-"\
+        "#{device.gsub(".", "-")}/#{[kind, phase].compact.join("-").downcase}/config"
+      client.publish(key, Oj.dump(data, mode: :compat), true)
+    end
+  end
+end
 
 get '/metrics' do
   response = Typhoeus::Request.new("#{URL}/api/last", timeout: 1).run
